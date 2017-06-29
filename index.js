f2bb6534981b9a3984f55c4b28a6246a60659469
nadajokezy <nadajokezyy@gmail.com>
2017-06-29 11:11:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -10,40 +10,52 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
   var speech = '';
-  var a = parseFloat(req.body.result.parameters.firstNum);
-  var b = parseFloat(req.body.result.parameters.secondNum);
-  var data;
   if(req.body.result.metadata.intentName === 'address'){
-      speech = 'Địa chỉ cửa hàng: '; 
+    speech = 'Địa chỉ cửa hàng: '; 
   }
   if(req.body.result.metadata.intentName === 'phone'){
-      speech = 'Liên hệ điện thoại: ';
+    speech = 'Liên hệ điện thoại: ';
   }
   if(req.body.result.action === 'unknown'){
-    speech = 'Should direct to level 2 bot';
-//     const https = require('https');
-//     const post_data = {
-//       query: req.body.resolvedQuery,
-//       sessionId: '18111996',
-//       lang: 'en'
-//     }
-//     const post_options = {
-//       host: 'api.api.ai',
-//       port: '80',
-//       path: '/api/query?v=20150910'
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json; charset=utf-8'
-//         'Authorization': 'Bearer c61e2d9d98cb42f6829946be5336be50'
-//       }
-//     }
-//     const post_req = https.request(post_options, function(res){
-//       res.on('data',function(chunk){
-//         speech = chunk.body.result.fulfillment.speech;
-//       });
-//     });
-//     post_req.write(post_data);
-//     post_req.end();
+    var query = (req.body.result.resolvedQuery..replace(/ /g,"%20"));
+    var request = require('request');
+    var options = {
+      url: 'https://api.api.ai/api/query?v=20150910&sessionId=18111996&query='+query,
+      headers: {
+        'Authorization': 'Bearer c61e2d9d98cb42f6829946be5336be50'
+      }
+    };
+ 
+    function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      speech = info.result.fulfillment.speech;
+    }
+  }
+  request(options, callback);
+    // const https = require('https');
+    // const post_data = {
+    //   query: req.body.resolvedQuery,
+    //   sessionId: '18111996',
+    //   lang: 'en'
+    // }
+    // const post_options = {
+    //   host: 'api.api.ai',
+    //   port: '80',
+    //   path: '/api/query?v=20150910'
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json; charset=utf-8'
+    //     'Authorization': 'Bearer c61e2d9d98cb42f6829946be5336be50'
+    //   }
+    // }
+    // const post_req = https.request(post_options, function(res){
+    //   res.on('data',function(chunk){
+    //     speech = chunk.body.result.fulfillment.speech;
+    //   });
+    // });
+    // post_req.write(post_data);
+    // post_req.end();
   }
   return res.json({
     speech: speech,
